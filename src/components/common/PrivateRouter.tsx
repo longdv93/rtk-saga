1d0e5efdf05789711050538e7afc98d8d919763d
longdv <dangvanlong26081993@gmail.com>
2022-04-01 16:36:57 +0700

    Feat: config router dom

---
+++ src/components/common/PrivateRouter.tsx
@@ -0,0 +1,9 @@
+import { Redirect, Route, RouteProps } from "react-router-dom";
+
+export function PrivateRouter(props: RouteProps) {
+
+  const isLoggedIn = Boolean(localStorage.getItem('access_token'));
+  if (!isLoggedIn) return <Redirect to='/login' />
+
+  return <Route {...props} />
+}
